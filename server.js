b7ed3509d7123225b2558ba570b3de9dec23b633
sanoobtv <sanoob.tv@gmail.com>
2017-04-29 17:34:45 +1000

    First Commit

---
+++ server.js
@@ -0,0 +1,9 @@
+console.log('node be with u')
+const express = require('express');
+const app=express();
+
+app.listen(3000,function() { console.log ('listening on 3000')})
+app.get('/', function (req,res) {
+
+	res.sendFile(__dirname+ '/index.html')
+})

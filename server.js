e5856b88a9b5ab7a5e7c75b9e80d3f061c2d254b
sanoobtv <sanoob.tv@gmail.com>
2017-04-29 21:44:29 +1000

    mongo

--- server.js
+++ server.js
@@ -1,9 +1,41 @@
+//logging a sentence
 console.log('node be with u')
+//
 const express = require('express');
+const bodyParser= require('body-Parser');
 const app=express();
+app.set('view engine','ejs')
+var db
+const MongoClient = require('mongodb').MongoClient
+MongoClient.connect('mongodb://admin:Tnp4ydaf@ds123331.mlab.com:23331/star-wars', (err,database) =>
+{
 
-app.listen(3000,function() { console.log ('listening on 3000')})
-app.get('/', function (req,res) {
+	if (err) return console.log(err)
+	db=database
+	app.listen(3000,function() { console.log ('listening on 3000')})
 
-	res.sendFile(__dirname+ '/index.html')
 })
+
+
+//urlencoded extract vdata from form
+app.use(bodyParser.urlencoded({extended: true}))
+app.get('/', function (req,res)
+	{
+		db.collection('quotes').find().toArray((err,result) =>
+{
+if(err) return console.log(err)
+res.render('index.ejs',{quotes:result})
+
+})
+	})
+
+app.post('/quotes', (req,res) =>
+	 {
+		console.log('You just pressed a button')
+		console.log(req.body)
+		db.collection('quotes').save(req.body, (err,result) => {
+			if(err) return console.log(err)
+			console.log("Record Added to DB")
+			res.redirect('/')
+		})
+	})
